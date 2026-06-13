f225f24f715a08aae3d847814c5150d2884fc277
SL.Ford <duytyler520@gmail.com>
2026-06-13 22:07:50 +0700

    init: backend structure

---
+++ backend/src/main/java/vn/edu/ut/pbms/ParkingBuildingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.ut.pbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBuildingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingBuildingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/vn/edu/ut/pbms/ParkingBuildingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.ut.pbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingBuildingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

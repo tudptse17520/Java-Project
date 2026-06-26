93648154d230304df1fe963be2e6a93061bd9e41
YukiSahra <nguyentranduykha2005@gmail.com>
2026-06-26 19:31:51 +0700

    feat: add manage parking floors

---
+++ backend/src/main/java/vn/edu/ut/pbms/dto/response/FloorResponseDTO.java
@@ -0,0 +1,22 @@
+package vn.edu.ut.pbms.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vn.edu.ut.pbms.constant.FloorStatus;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FloorResponseDTO {
+    private Long id;
+    private String floorName;
+    private Integer floorLevel;
+    private Integer capacity;
+    private FloorStatus status;
+    private Long buildingId;
+    private String buildingName;
+    private Long vehicleTypeId;
+}

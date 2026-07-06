88e0cc91e9a8514c451ab636ee464d4b8639a6df
TranChiTrung <chitrung1357@gmail.com>
2026-07-06 22:10:57 +0700

    chore(config): setup async, cors and modelmapper configurations

---
+++ backend/src/main/java/vn/edu/ut/pbms/config/ModelMapperConfig.java
@@ -0,0 +1,20 @@
+package vn.edu.ut.pbms.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true);
+        return modelMapper;
+    }
+}

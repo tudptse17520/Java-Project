263067bb45a6f1a66ff2b11a49f81fd6940fe1d2
TMAcoder <trunn1510@gmail.com>
2026-07-12 03:13:50 +0700

    fix(backend): remove unused imports and variables in checkout module

--- backend/src/main/java/vn/edu/ut/pbms/dto/request/LostTicketRequest.java
+++ backend/src/main/java/vn/edu/ut/pbms/dto/request/LostTicketRequest.java
@@ -1,7 +1,6 @@
 package vn.edu.ut.pbms.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- backend/src/main/java/vn/edu/ut/pbms/dto/request/OverrideCheckoutRequest.java
+++ backend/src/main/java/vn/edu/ut/pbms/dto/request/OverrideCheckoutRequest.java
@@ -2,7 +2,7 @@ package vn.edu.ut.pbms.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

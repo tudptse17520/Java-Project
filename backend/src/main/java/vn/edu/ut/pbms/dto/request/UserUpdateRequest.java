c668f6b5863599b2fddd3527bc182e41772f9807
tudptse17520 <tudptse17520@gmail.com>
2026-07-12 02:47:20 +0700

    feat(user): add regex validation for phone and username length

--- backend/src/main/java/vn/edu/ut/pbms/dto/request/UserUpdateRequest.java
+++ backend/src/main/java/vn/edu/ut/pbms/dto/request/UserUpdateRequest.java
@@ -1,6 +1,7 @@
 package vn.edu.ut.pbms.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class UserUpdateRequest {
     private String fullName;
 
     @NotBlank(message = "Số điện thoại không được để trống.")
+    @Pattern(regexp = "^(0[3|5|7|8|9])+([0-9]{8})$", message = "Số điện thoại không đúng định dạng.")
     private String phoneNumber;
 
     @NotBlank(message = "Vai trò không được để trống.")
